90a2e28cf17ea21fe43d084dd1a63b86601f63a4
Garth Leedle <g.leedle@students.clark.edu>
2020-05-04 16:46:40 -0700

    Done

--- inc/upload.inc.php
+++ inc/upload.inc.php
@@ -1,35 +1,35 @@
 <?php
 
-    // Define these errors in an array
-    $upload_errors = array(
-        UPLOAD_ERR_OK                 => "No errors.",
-        UPLOAD_ERR_INI_SIZE          => "Larger than upload_max_filesize.",
-        UPLOAD_ERR_FORM_SIZE         => "Larger than form MAX_FILE_SIZE.",
-        UPLOAD_ERR_PARTIAL             => "Partial upload.",
-        UPLOAD_ERR_NO_FILE             => "No file.",
-        UPLOAD_ERR_NO_TMP_DIR         => "No temporary directory.",
-        UPLOAD_ERR_CANT_WRITE         => "Can't write to disk.",
-        UPLOAD_ERR_EXTENSION         => "File upload stopped by extension."
-    );
+// Define these errors in an array
+$upload_errors = array(
+    UPLOAD_ERR_OK                 => "No errors.",
+    UPLOAD_ERR_INI_SIZE          => "Larger than upload_max_filesize.",
+    UPLOAD_ERR_FORM_SIZE         => "Larger than form MAX_FILE_SIZE.",
+    UPLOAD_ERR_PARTIAL             => "Partial upload.",
+    UPLOAD_ERR_NO_FILE             => "No file.",
+    UPLOAD_ERR_NO_TMP_DIR         => "No temporary directory.",
+    UPLOAD_ERR_CANT_WRITE         => "Can't write to disk.",
+    UPLOAD_ERR_EXTENSION         => "File upload stopped by extension."
+);
 
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-        // what file do we need to move?
-        $tmp_file = $_FILES['file_upload']['tmp_name'];
+    // what file do we need to move?
+    $tmp_file = $_FILES['file_upload']['tmp_name'];
 
-        // set target file name
-        // basename gets just the file name
-        $target_file = basename($_FILES['file_upload']['name']);
+    // set target file name
+    // basename gets just the file name
+    $target_file = basename($_FILES['file_upload']['name']);
 
-        // set upload folder name
-        $upload_dir = 'uploads';
+    // set upload folder name
+    $upload_dir = 'uploads';
 
-        // Now lets move the file
-        // move_uploaded_file returns false if something went wrong
-        if (move_uploaded_file($tmp_file, $upload_dir . "/" . $target_file)) {
-            $message = "File uploaded successfully";
-        } else {
-            $error = $_FILES['file_upload']['error'];
-            $message = $upload_errors[$error];
-        } // end of if
+    // Now lets move the file
+    // move_uploaded_file returns false if something went wrong
+    if (move_uploaded_file($tmp_file, $upload_dir . "/" . $target_file)) {
+        $message = "File uploaded successfully";
+    } else {
+        $error = $_FILES['file_upload']['error'];
+        $message = $upload_errors[$error];
     } // end of if
+} // end of if
--- upload-files.php
+++ upload-files.php
@@ -1,6 +1,6 @@
-<?php 
-    require "inc/upload.inc.php";
-    require "inc/display.inc.php";    
+<?php
+require "inc/upload.inc.php";
+require "inc/display.inc.php";
 ?>
 <!DOCTYPE html>
 <!-- 
@@ -23,25 +23,24 @@
 </head>
 
 <body>
-<?php include "inc/navbar.inc.php" ?>
-<div class="concert-bg"><img class="concert-bg" src="img/concert.jpg" alt="Concert"></div>
-<br>
-<h1>Welcome to the Concert photo uploader from &copy;Fractal Productions</h1>
-<div class="main-text">
-<p class="uploader-text">This is an area that members can use to show off their great photos of artists and of concerts that you've attended. Please make sure all photos uploaded are appropriate and are family friendly. </p>
-</div>
-<div class="container">
+    <?php include "inc/navbar.inc.php" ?>
+    <div class="concert-bg"><img class="concert-bg" src="img/concert.jpg" alt="Concert"></div>
+    <br>
+    <h1>Welcome to the Concert photo uploader from &copy;Fractal Productions</h1>
+    <div class="main-text">
+        <p class="uploader-text">This is an area that members can use to show off their great photos of artists and of concerts that you've attended. Please make sure all photos uploaded are appropriate and are family friendly. </p>
+    </div>
+    <div class="container">
         <form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
-            <div class="container">            
+            <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="form-group">                            
+                        <div class="form-group">
                             <label class="control-label text-center">Upload Your concert photos</label>
                             <div class="preview-zone hidden">
                                 <div class="box box-solid">
                                     <div class="box-header with-border">
                                         <div><b>Preview</b></div>
-                                        
                                     </div>
                                     <div class="box-body"></div>
                                 </div>
@@ -61,9 +60,9 @@
                     <div class="col-md-12">
                         <button type="submit" class="btn btn-primary pull-right">Upload</button>
                         <div class="box-tools pull-right">
-                                            <button type="button" class="btn btn-danger btn-xs remove-preview">
-                                                <i class="fa fa-times"></i>Reset Form</button>
-                                        </div>
+                            <button type="button" class="btn btn-danger btn-xs remove-preview">
+                                <i class="fa fa-times"></i>Reset Form</button>
+                        </div>
                         <?php
                         if (!empty($message)) {
                             echo "<p id=\"alert\" class=\"alert alert-primary mt-4 text-center\">{$message}</p>";
@@ -84,7 +83,7 @@
     </div>
 
     <script src="js/script.js"></script>
-</body>    
+</body>
 
 
 
