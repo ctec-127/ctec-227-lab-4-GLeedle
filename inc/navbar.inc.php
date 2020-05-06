3c55a94a1e3553b79b189653235a5fee80a66694
Garth Leedle <g.leedle@students.clark.edu>
2020-05-05 17:10:53 -0700

    1

--- inc/navbar.inc.php
+++ inc/navbar.inc.php
@@ -1,9 +1,22 @@
 <nav class="nav shadow">
+<ul class="nav">
+<li class="nav-item">
   <a class="nav-link active" href="https://win2008.clarkwp.com/"><img src="img/fractal-logo.png" alt="Fractal Productions logo" height="56"> FRACTAL PRODUCTIONS</a>
+</li>
+  <li class="nav-item">
   <a class="nav-link mt-3" href="https://win2008.clarkwp.com/">Home</a>
+  </li>
+  <li class="nav-item">
   <a class="nav-link mt-3" href="https://win2008.clarkwp.com/artist-updates/">Artist Updates</a>
+  </li>
+  <li class="nav-item">
   <a class="nav-link mt-3" href="https://win2008.clarkwp.com/contact-page/">Contact Page</a>
+  </li>
+  <li class="nav-item">
   <a class="nav-link mt-3" href="https://win2008.clarkwp.com/about/">About</a>
+  </li>
+  <li class="nav-item">
   <a class="nav-link mt-3" href="https://win2008.clarkwp.com/newsletter/">Newsletter</a>
-  
+  </li>
+</ul>
 </nav>
